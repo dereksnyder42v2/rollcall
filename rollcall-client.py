7b848efe46d0a24d3165cf608065c982b32c28a1
derek <dereksnyder42@gmail.com>
2020-03-14 00:12:27 -0600

    first take on how the client might work

--- rollcall-client.py
+++ rollcall-client.py
@@ -1,5 +1,19 @@
+#!/usr/bin/python3
+
 import requests
+import socket
 
-with open('test.txt', 'w') as f:
-    f.write('hello, world! requests library imported.\n')
+_SERVER_ADDR = "10.0.0.201/cgi-bin/rollcall.py"
 
+def sendUpdate():
+    payload = {'hostname': socket.gethostname()}
+    r = requests.get("http://" + _SERVER_ADDR, params=payload)
+    print("Request url was:", r.url)
+    return r.text
+
+def main():
+    resp = sendUpdate()
+    print(resp)
+
+if __name__ == "__main__":
+    main()
